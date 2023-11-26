5ef942c5585b8b9341a0cd9f90aab0b79371fd90
kaicong12 <kaicongt@knownuggets.com>
2023-11-26 19:03:01 +0800

    moved sitemap and robots.txt into app directory

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export const metadata = {
   description: "Perniagaan Motor Kekal is Johor Bahru's trusted motorcycle dealer offering sales, repairs, and accessories for brands like Yamaha and Kawasaki.",
   keywords: [
       'kedai motor', 'motor shop', 'motorcycle', 'yamaha dealer', 'kawasaki dealer',
-      'motor repair', 'LC135', 'motor shop johor jaya', 'kedai motor johor bahru'
+      'motor repair', 'LC135', 'motor shop johor jaya', 'kedai motor johor bahru', 'kedai jual motor johor jaya'
   ]
 };
 
